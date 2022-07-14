b2bd6ff2c59a3f21f436b1ccd3848107060a88dc
freeve4h <freeve4h@outlook.com>
2022-07-14 13:38:56 -0400

    yes

---
+++ Player.lua
@@ -0,0 +1,48 @@
+Player = {}
+
+function Player:load()
+    Player.xpos = 400
+    Player.ypos = 300
+    Player.Size = 32
+    Player.Width = Player.Size
+    Player.Height = Player.Size
+    Player.Speed = 400 -- yes
+end
+
+function Player:update(dt)
+    Player:Bounderies()
+    -- moving and stuff
+    if love.keyboard.isDown("up") then 
+        Player.ypos = Player.ypos - Player.Speed * dt 
+    end
+    if love.keyboard.isDown("down") then 
+        Player.ypos = Player.ypos + Player.Speed * dt -- this is very interesting content, not boring at all...
+    end
+    if love.keyboard.isDown("right") then 
+        Player.xpos = Player.xpos + Player.Speed * dt -- this must be getting very boring 
+    end
+    if love.keyboard.isDown("left") then 
+        Player.xpos = Player.xpos - Player.Speed * dt -- i am commenting on my code for no reason at all
+    end
+    Player:Bounderies()
+end
+
+function Player:draw()
+    love.graphics.rectangle("fill", Player.xpos, Player.ypos, Player.Width, Player.Height)
+end
+
+function Player:Bounderies()
+    -- my pc is so bad
+    if Player.xpos < 0 then 
+        Player.xpos = 0
+    end
+    if Player.ypos < 0 then 
+        Player.ypos = 0 
+    end
+    if Player.xpos + Player.Width > love.graphics.getWidth() then 
+        Player.xpos = love.graphics.getWidth() - Player.Width 
+    end
+    if Player.ypos + Player.Height > love.graphics.getHeight() then -- besure to watch my other content!!!
+        Player.ypos = love.graphics.getHeight() - Player.Height 
+    end
+end
--- main.lua
+++ main.lua
@@ -1,11 +1,19 @@
 require("stolencode")
+require("Player")
 
 function love.load()
     love.window.setMode(800, 600, {resizable = false})
+    Player:load()
 end
 
 function love.update(dt)
+    Player:update(dt)
+    -- to quit the game of something idk i just need to record content
+    if love.keyboard.isDown("escape") then 
+        love.event.quit()
+    end
 end
 
 function love.draw()
+    Player:draw()
 end
